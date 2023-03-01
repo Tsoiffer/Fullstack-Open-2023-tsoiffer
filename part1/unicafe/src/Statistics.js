323184c1d6c03c6af965475717f8d4daa5d85304
tsoiffer <tsoiffer@frba.utn.edu.ar>
2023-03-01 10:56:36 -0300

    se carga ejercicio 1.8

---
+++ part1/unicafe/src/Statistics.js
@@ -0,0 +1,16 @@
+export const Statistics = ({ good, neutral, bad }) => {
+	const all = good + bad + neutral;
+	const average = all === 0 ? 0 : (good - bad) / all;
+	const positive = all === 0 ? 0 : (good / all) * 100;
+	return (
+		<>
+			<h1>statics</h1>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+			<p>all {all}</p>
+			<p>average {average}</p>
+			<p>positive {positive}%</p>
+		</>
+	);
+};
