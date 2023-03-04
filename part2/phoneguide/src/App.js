ba7c17d9675c1c1ac4489e079b3cf8ab976bf133
tsoiffer <tsoiffer@frba.utn.edu.ar>
2023-03-04 16:51:20 -0300

    se carga ejercicio 2.11

--- part2/phoneguide/src/App.js
+++ part2/phoneguide/src/App.js
@@ -1,54 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-const initialPersons = [
-  { name: "Arto Hellas", number: "040-123456" },
-  { name: "Ada Lovelace", number: "39-44-5323523" },
-  { name: "Dan Abramov", number: "12-43-234345" },
-  { name: "Mary Poppendieck", number: "39-23-6423122" },
-];
+import axios from "axios";
 const App = () => {
-  const [persons, setPersons] = useState(initialPersons);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [toFilter, setfilter] = useState("");
+	const [persons, setPersons] = useState([]);
+	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
+	const [toFilter, setfilter] = useState("");
 
-  const addPerson = (event) => {
-    event.preventDefault();
-    setPersons(persons.concat({ name: newName, number: newNumber }));
-    setNewName("");
-    setNewNumber("");
-  };
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-  const handleFilterChange = (event) => {
-    setfilter(event.target.value);
-  };
+	useEffect(() => {
+		console.log("effect");
+		axios.get("http://localhost:3001/persons").then((response) => {
+			console.log("promise fulfilled");
+			setPersons(response.data);
+		});
+	}, []);
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter
-        toFilter={toFilter}
-        handleFilterChange={handleFilterChange}
-      ></Filter>
-      <h3>add a new</h3>
-      <PersonForm
-        addPerson={addPerson}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
-      <h3>Numbers</h3>
-      <Persons persons={persons} toFilter={toFilter} />
-    </div>
-  );
+	const addPerson = (event) => {
+		event.preventDefault();
+		setPersons(persons.concat({ name: newName, number: newNumber }));
+		setNewName("");
+		setNewNumber("");
+	};
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
+	};
+	const handleFilterChange = (event) => {
+		setfilter(event.target.value);
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<Filter
+				toFilter={toFilter}
+				handleFilterChange={handleFilterChange}
+			></Filter>
+			<h3>add a new</h3>
+			<PersonForm
+				addPerson={addPerson}
+				newName={newName}
+				handleNameChange={handleNameChange}
+				newNumber={newNumber}
+				handleNumberChange={handleNumberChange}
+			/>
+			<h3>Numbers</h3>
+			<Persons persons={persons} toFilter={toFilter} />
+		</div>
+	);
 };
 
 export default App;
